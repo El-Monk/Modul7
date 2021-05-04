53e0f75be2a620407ad4bc747d0fe491ca66d465
Monk <elmonk.drb@yandex.ru>
2021-05-04 16:55:14 +0300

    HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("v2");
+        Warehouse warehouse = new Warehouse();
+        Picker picker = new Picker(warehouse);
+        Courier courier = new Courier(warehouse);
+
+        //проверка баланса
+        System.out.println(warehouse + " " + picker + " " + courier);
+
+        //проверка баланка после первого действия
+        picker.doWork();
+        courier.doWork();
+        System.out.println(warehouse + " " + picker + " " + courier);
+
+        //проверка после 1500 действий
+        for (int i = 1; i < 1500; i++) {
+            picker.doWork();
+            courier.doWork();
+        }
+        picker.bonus();
+        courier.bonus();
+        System.out.println(warehouse + " " + picker + " " + courier);
+
+        //проверка после 1501 дейставия
+        picker.doWork();
+        courier.doWork();
+        picker.bonus();
+        courier.bonus();
+        System.out.println(warehouse + " " + picker + " " + courier);
+
+
     }
 }
